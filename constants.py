3c863ff3bd0116fce0adc241aab208bcde8d9f13
Giovanni Calzolari <giovannicalzolari@outlook.com>
2020-11-11 08:42:55 +0100

    Changed readmefile and changed some stuff of the code

---
+++ constants.py
@@ -0,0 +1,12 @@
+
+class color:
+    PURPLE = '\033[95m'
+    CYAN = '\033[96m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    END = '\033[0m'
+    ITA = '\x1B[3m'
--- sve.py
+++ sve.py
@@ -1,63 +1,61 @@
 import pandas as pd
+from constants import *
+
+
 pd.options.mode.chained_assignment = None  # default='warn'
 import random as rd
 from datetime import datetime
 from datetime import date
 import math
 
-
 df = pd.read_csv('GiosDict.txt')
 # df = pd.read_csv('~/Dropbox/svenska/GiosDict.txt')
 
-today = date.today()
-today = today.strftime("%Y-%m-%d")
 
 def days_between(d1, d2):
     d1 = datetime.strptime(d1, "%Y-%m-%d")
     d2 = datetime.strptime(d2, "%Y-%m-%d")
     return abs((d2 - d1).days)
 
-class color:
-    PURPLE = '\033[95m'
-    CYAN = '\033[96m'
-    BLUE = '\033[94m'
-    GREEN = '\033[92m'
-    YELLOW = '\033[93m'
-    RED = '\033[91m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-    END = '\033[0m'
-    ITA = '\x1B[3m'
+def RemovePoints(data):
+    today = date.today()
+    today = today.strftime("%Y-%m-%d")
+    for i in range(0, len(data)):
+        dd = days_between(today,data["day"].iloc[i])
+        k = dd // 7
+        data["point"].iloc[i] = data["point"].iloc[i] - k
+        if data["point"].iloc[i] < 0:
+            data["point"].iloc[i] = 0
+        data.to_csv('GiosDict.txt',index=False)
 
-    
-for i in range(0,len(df)):
-    dd = days_between(today,df["day"].iloc[i])
-    k = math.floor(dd/7)
-    df["point"].iloc[i] = df["point"].iloc[i] - k
-    if df["point"].iloc[i] < 0:
-        df["point"].iloc[i] = 0
-    df.to_csv('GiosDict.txt',index=False)
+RemovePoints(df)
 
 print(color.CYAN+color.BOLD+'\n WELCOME!\n'+color.END)
 colnames = ["word","point","meaning","class","day"]
 # main menu
 
-j = 1
-while j == 1:
+def quitorgoback(action, logic, backorquit):
+    if action.startswith(backorquit):
+        logic = False
+
+mainloop = True
+
+
+while mainloop:
 
     action = input('What do you want to do? (add/play/see/quit) ').lower()
 
-# add new words
+    # add new words
     if action.startswith('a'):
-        i = 1
-        while i == 1:
+        Add = True
+        while Add:
             df =pd.read_csv('GiosDict.txt')
             inpt = input('Insert a new word, meaning, class, or enter '+color.ITA+'back\n '+color.END)
 
             if not inpt:
                 print('You have not inserted anything =( \n')
             elif inpt in 'back':
-                i = i+1
+                Add = False
             else:
 
                 ind = inpt.split(',')
@@ -65,7 +63,7 @@ while j == 1:
                 df.to_csv('GiosDict.txt',index=False)
                 
                 if len(ind) == 1:
-                    inpt = input('Wanna add meaning or class? enter if not \n ')
+                    inpt = input('Wanna add meaning or class? enter if not \n ').lower()
                     if not inpt:
                         m = None
                         cl = None
@@ -73,14 +71,14 @@ while j == 1:
                         ind = inpt.split(',')
                         m = inpt[0]
                         if len(ind) == 1:
-                            inpt = input('Wanna add a class? enter if not \n ')
+                            inpt = input('Wanna add a class? enter if not \n ').lower()
                         if not inpt:
                             cl = None
                         else:
                             cl = inpt
                 elif len(ind) == 2:
                     m = ind[1]
-                    inpt('Wanna add a class? enter if not \n ')
+                    inpt = input('Wanna add a class? enter if not \n ').lower()
                     if not inpt:
                         cl = None
                     else:
@@ -89,9 +87,9 @@ while j == 1:
                     m = ind[1]
                     cl = ind[2]
                 
-                if cl == 'n' or cl == 'N':
+                if cl.startswith('n'):
                     cl = 'noun'
-                elif cl == 'v' or cl == 'V':
+                elif cl.startswith('v'):
                     cl = 'verb'
             
                 df2 = pd.DataFrame([[ordet,0,m,cl,today]],columns=colnames)
@@ -101,10 +99,10 @@ while j == 1:
                 df.to_csv('GiosDict.txt',index=False)
                 print(' added the word number'+color.BOLD+color.CYAN+' {} '.format(len(df))+color.END+'\n ')
                 
-# play a game
+    # play a game
     elif action.startswith('p'):
-        i = 1
-        while i == 1:
+        play = True
+        while play:
             # take the list and look which words has maximum points of 5            
             df =pd.read_csv('GiosDict.txt')
 
@@ -128,13 +126,16 @@ while j == 1:
 
             df.to_csv('GiosDict.txt',index=False)
             if ans.startswith('b'):
-                i = i + 1
+                play = False
 
-        print(df[['word','point']])
+        # print(df[['word','point']])
 
-# see more stats        
+    # see more stats        
     elif action.startswith('s'):
-        print(df[['word','meaning','class']])
+        
+        for i in range(len(df)):
+            df['word'].iloc[i]
+            
         ans=input("Do you see more, edit some words, or reset the points? (more/edit/reset/back) ").lower()
         if ans.startswith('r'):
             ans2 = input('Are you sure? ').lower()
@@ -163,8 +164,8 @@ while j == 1:
                     df['word'].iloc[ind] = newword
             
     elif action.startswith('q'):
-       j = j+1
-       print(' bye bitch <3')
+        mainloop = False
+        print(' bye bitch <3')
     else:
         print("not good")
     
